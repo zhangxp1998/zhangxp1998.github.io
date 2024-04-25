d7422b2046e41e3474e24aa2b59530cd78b2b5af
Kelvin Zhang <zhangxp1998@gmail.com>
2024-04-24 23:38:10 -0700

    Add new font

---
+++ larp/piaogen/data.js
@@ -0,0 +1,10 @@
+data = [{ "drama-name": "惊华" , "shop-name": ""}];
+
+window.onload = function () {
+  let drama_name = document.getElementById("drama-name");
+  let shop_name = document.getElementById("shop_name");
+  let name = document.getElementById("name");
+  let actor = document.getElementById("actor");
+  let character = document.getElementById("character");
+  let slogan = document.getElementById("slogan");
+};
